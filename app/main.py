3521bb8da73cca461a2f0f783fa516e7b4794831
lasheralberto <72318269+lasheralberto@users.noreply.github.com>
2022-03-24 22:59:20 +0100

    Flask restful API Hotels

--- app/main.py
+++ app/main.py
@@ -1,16 +1,12 @@
-from backend import Traveller
-
-#Traveller.get_cities_for_db(
- #  '3ccd09de80msh288908a563dc4f2p1129c0jsn2c47d1d1d79c'
-#)
 from flask import Flask
 import flask_restful as restful
 from flask_restful import Api, Resource
-
+from flask_cors import CORS
 
 import json
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
  
 # Opening JSON file
 json_file_hotels = open('my_data.json')
@@ -36,8 +32,8 @@ class Hotel(Resource):
     #return next(item for item in hotels if item["address.postalCode"] == cityId)
 
 
-api.add_resource(HotelList, '/hotels')
-api.add_resource(Hotel, '/hotels/<int:cityId>')
+api.add_resource(HotelList, '/')
+api.add_resource(Hotel, '/<int:cityId>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
