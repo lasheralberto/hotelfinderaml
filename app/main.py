66f7e6ae02b53ed2ee7793c60bd447b469a3853c
lasheralberto <72318269+lasheralberto@users.noreply.github.com>
2022-03-25 22:58:11 +0100

    Flask API

--- app/main.py
+++ app/main.py
@@ -2,35 +2,43 @@ from flask import Flask
 import flask_restful as restful
 from flask_restful import Api, Resource
 from flask_cors import CORS
-
+import pymongo
+from flask_pymongo import PyMongo
 import json
+import json
+from bson import json_util
+
 app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb://localhost/mongodbhotels'
+mongo = PyMongo(app)
 api = Api(app)
 CORS(app)
- 
+
+
+client = pymongo.MongoClient("mongodb+srv://lasheralberto:economiaUm94@cluster0.ov3vg.mongodb.net/mongodbhotels?retryWrites=true&w=majority")
+
 # Opening JSON file
-json_file_hotels = open('my_data.json')
- 
+#json_file_hotels = open(r'\Users\Alberto\OneDrive\Escritorio\flutter_ebook\flask-jsonapi\my_data.json')
 # returns JSON object as
 # a dictionary
-hotels = json.load(json_file_hotels)
-
-
+#hotels = json.load(json_file_hotels)
 
 class HotelList(Resource):
 
-   def get(self ):
-      return hotels, 200
+   def get(self):
+      hotels = list(client['mongodbhotels']['TravelAPI'].find())
+      json_response = json.dumps(hotels, default=json_util.default)
+      return json.loads(json_response), 200
 
 class Hotel(Resource):
   def get(self, cityId):
-      listareturn = []
-      for item in hotels:
-        if item["address.postalCode"] == cityId:
-           listareturn.append(item)
-      return listareturn
-    #return next(item for item in hotels if item["address.postalCode"] == cityId)
-
+    
+      filter={
+      'address.postalCode': cityId
+      }
+      docs_list  = list(client['mongodbhotels']['TravelAPI'].find(filter = filter))
+      json_response = json.dumps(docs_list, default=json_util.default)
+      return json.loads(json_response)
 
 api.add_resource(HotelList, '/')
 api.add_resource(Hotel, '/<int:cityId>')
