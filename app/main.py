2dac87aa68cc9c958becb2d220ffed887430639d
lasheralberto <72318269+lasheralberto@users.noreply.github.com>
2022-03-26 17:44:51 +0100

    Flask

--- app/main.py
+++ app/main.py
@@ -5,43 +5,41 @@ from flask_cors import CORS
 import pymongo
 from flask_pymongo import PyMongo
 import json
-import json
 from bson import json_util
+import dns
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb://localhost/mongodbhotels'
+app.config['MONGO_URI'] = "mongodb+srv://lasheralberto:economiaUm94@cluster0.ov3vg.mongodb.net/mongodbhotels?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 api = Api(app)
 CORS(app)
 
 
-client = pymongo.MongoClient("mongodb+srv://lasheralberto:economiaUm94@cluster0.ov3vg.mongodb.net/mongodbhotels?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://lasheralberto:economiaUm94@cluster0.ov3vg.mongodb.net/mongodbhotels?retryWrites=true&w=majority", connect= False)
 
 # Opening JSON file
 #json_file_hotels = open(r'\Users\Alberto\OneDrive\Escritorio\flutter_ebook\flask-jsonapi\my_data.json')
-# returns JSON object as
-# a dictionary
 #hotels = json.load(json_file_hotels)
 
 class HotelList(Resource):
 
    def get(self):
-      hotels = list(client['mongodbhotels']['TravelAPI'].find())
+      hotels = list(client['mongodbhotels']['weather_conditions'].find())
       json_response = json.dumps(hotels, default=json_util.default)
       return json.loads(json_response), 200
 
 class Hotel(Resource):
-  def get(self, cityId):
+  def get(self, cityName):
     
       filter={
-      'address.postalCode': cityId
+      'name': cityName.capitalize()
       }
-      docs_list  = list(client['mongodbhotels']['TravelAPI'].find(filter = filter))
+      docs_list  = list(client['mongodbhotels']['wheather_conditions'].find(filter = filter))
       json_response = json.dumps(docs_list, default=json_util.default)
       return json.loads(json_response)
 
 api.add_resource(HotelList, '/')
-api.add_resource(Hotel, '/<int:cityId>')
+api.add_resource(Hotel, '/<string:name>')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
